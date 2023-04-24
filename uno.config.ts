cc3efeff6d35df4faf3e0a45affcbf2d73e29e17
Geoff Dick <git@geoffdick.com>
2023-04-24 19:45:28 +0000

    feat: add fonts from Fontsource

--- uno.config.ts
+++ uno.config.ts
@@ -1,54 +1,35 @@
 // uno.config.ts
 import { defineConfig, presetTypography, presetUno } from "unocss";
 import presetTheme from "unocss-preset-theme";
+import transformerDirectives from "@unocss/transformer-directives";
 import type { Theme } from "unocss/preset-uno";
 
 export default defineConfig<Theme>({
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: '"Roboto FlexVariable", sans-serif',
+      display: '"Radio CanadaVariable", sans-serif',
+    },
+  },
   presets: [
     // https://unocss.dev/presets/uno
     presetUno(),
 
     // https://unocss.dev/presets/typography
-    presetTypography(),
+    presetTypography({
+      cssExtend: {
+        "h1,h2,h3,h4,h5,h6": {
+          "font-weight": "700",
+        },
+      },
+    }),
 
     // https://github.com/Dunqing/unocss-preset-theme
     presetTheme<Theme>({
       theme: {
-        modern: {},
-        twentyoughts: {
-          colors: {
-            base: "#33b1e5",
-            content: "#2d52a4",
-            primary: "#33b1e5",
-            secondary: "#33b1e5",
-            accent: "#33b1e5",
-          },
-        },
-        nineteennineties: {
-          colors: {
-            base: "#ffffff",
-            content: "#000000",
-            primary: "#8f7ee6",
-            secondary: "#f0d133",
-            accent: "#edb4da",
-          },
-        },
-        nineteeneighties: {
-          fontFamily: {
-            sans: "1980s",
-          },
-          colors: {
-            base: "#140627",
-            content: "#ffffff",
-            primary: "#f0d133",
-            secondary: "#8f7ee6",
-            accent: "#edb4da",
-          },
-        },
-        nineteenseventies: {},
-        nineteessixties: {},
+        dark: {},
       },
     }),
   ],
+  transformers: [transformerDirectives()],
 });
