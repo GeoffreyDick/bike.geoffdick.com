2d1b595f4ff90bd07a54bcf663a76e713b5f9b4f
Geoff Dick <git@geoffdick.com>
2023-04-27 15:59:07 +0000

    feat: add api route to convert gpx to geojson

--- src/pages/api/[filename].geojson.ts
+++ src/pages/api/[filename].geojson.ts
@@ -1,43 +1,52 @@
-import type { APIRoute } from "astro";
-import parser from "@bovan/gpx2geojson";
-import { gpx } from "@tmcw/togeojson";
-import { DOMParser } from "xmldom";
-import { readFile } from "node:fs/promises";
+import type { APIRoute } from 'astro';
+import { gpx } from '@tmcw/togeojson';
+import { DOMParser } from 'xmldom';
+import { readFile } from 'node:fs/promises';
 
-async function parseGpx(path: string): Promise<GeoJSON.GeoJsonObject> {
-  let gpxData: string;
-    const file = await readFile(path, "utf-8");
-    gpxData = new DOMParser().parseFromString(file, "text/xml");
-    return gpx(gpxData);
+async function parseGpx(path: string) {
+	let file: string;
+	try {
+		file = await readFile(path, 'utf-8');
+	} catch (error: any) {
+		throw new Error(error?.message);
+	}
+	const gpxData = new DOMParser().parseFromString(file, 'text/xml');
+	return JSON.stringify(gpx(gpxData));
 }
 
 export const get: APIRoute = async function get({ params }) {
-  const { file } = params;
+	const { filename } = params;
 
-  if (!file) {
-    return {
-      status: 400,
-      body: "parameter 'file' not found",
-    };
-  }
+	if (!filename) {
+		return {
+			status: 400,
+			body: "parameter 'filename' not found",
+		};
+	}
 
-  const geojson = await parseGpx(file);
-  return {
-    status: 200,
-    body: geojson,
-    headers: {
-      "Content-Type": "application/geo+json",
-    },
-  };
+	const filePath = `./src/assets/gpx/${filename}.gpx`;
+
+	const geojson = await parseGpx(filePath);
+	return {
+		status: 200,
+		body: geojson,
+		headers: {
+			'Content-Type': 'application/geo+json',
+		},
+	};
 };
 
 export async function getStaticPaths() {
-  const files = import.meta.glob("/src/assets/gpx/*.gpx");
-  return files.keys().map((file: string) => {
-    return {
-      params: {
-        file,
-      },
-    };
-  });
+	const glob = import.meta.glob('/src/assets/gpx/*.gpx', { as: 'raw', eager: true });
+	let files: string[] = [];
+	for (let key in glob) {
+		files.push(key);
+	}
+	return files.map((file: string) => {
+		return {
+			params: {
+				filename: file.replace('/src/assets/gpx/', '').replace('.gpx', ''),
+			},
+		};
+	});
 }
