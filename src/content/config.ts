c673f4367ffb056a9c7253067d0601717cdd6614
Geoff Dick <git@geoffdick.com>
2023-04-29 09:59:57 -0600

    feat: organize and update content (#3)
    * chore: remove placeholder image
    
    * feat: organize and update content
    
    * fix: update header link to races
    
    * fix: add untracked files

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,39 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const posts = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-		gpxFiles: z.array(z.string().startsWith('/src/assets/gpx/').endsWith('.gpx')),
-	}),
+const seoFields = {
+  title: z.string(),
+  description: z.string(),
+  image: z.string().url().optional(),
+};
+
+const postFields = {
+  pubDate: z
+    .string()
+    .or(z.date())
+    .transform((val) => new Date(val)),
+  updatedDate: z
+    .string()
+    .optional()
+    .transform((str) => (str ? new Date(str) : undefined)),
+};
+
+const races = defineCollection({
+  schema: z.object({
+    ...seoFields,
+    ...postFields,
+    gpxFiles: z.array(
+      z.string().startsWith("/src/assets/gpx/").endsWith(".gpx"),
+    ),
+    links: z.object({
+      strava: z.array(z.string().url()).min(1),
+      trackleaders: z.string().url(),
+    }),
+    result: z.object({
+      status: z.enum(["Finished", "Scratched"]),
+      place: z.number().positive().int().optional(),
+      time: z.string().optional(),
+    }),
+  }),
 });
 
-export const collections = { posts };
+export const collections = { races };
